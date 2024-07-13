05f6216d8ef2e682879d4baf2d88f192c4580c71
lily <lilyjulymm2009@gmail.com>
2024-07-13 13:48:30 +0630

    Readme.md

---
+++ Readme.md
@@ -0,0 +1,46 @@
+# Student's Bio
+
+Tools:
+- HTML
+    - Pseudo Class / Pseudo Element
+    - Attributes
+    - Semantic HTMl
+- CSS
+    - Universal / :root selector
+    - CSS Pesudo
+    - ComputerStyle
+- JavaScript
+    - getElementById
+    - Function
+    - conditional statment
+- Google Chrone
+    - browser
+    - dew tools
+- VS Code
+    - IDE
+    - Extentions
+- Font Awescme / cdnjs
+    - other necessary icons
+- Git
+   - git workflow
+- GitHub
+  - github repo
+  - github issue
+  - github pages
+
+## HTML Structure
+
+
+## Mockup
+
+
+
+## License
+MIT
+
+
+## Contact Me
+- Email:
+- GitHub:
+
+Made with love by me,2024.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,34 @@
+
 const img = document.getElementById("mybio");
 img.addEventListener("click", changeShape);
 
 const shapeArr = [
-     //Paralleogon
-     "25% 0%, 100% 0%, 75% 100%, 0% 100%",
-     // Trapezoid
-     "20% 0%, 80% 0%, 100% 100%, 0% 100%",
-     // Left point
-     "25% 0%, 100% 0%, 100% 100%, 25% 100%, 0% 50%",
-     // Frame
-     "0% 0%, 0% 100%, 25% 100%, 12% 12%, 92% 11%, 91% 90%, 0 91%, 25% 100%, 100% 100%, 100% 0%",
+      // Hexagon
+    "25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%",
+    //Paralleogon
+    "25% 0%, 100% 0%, 75% 100%, 0% 100%",
+    // Trapezoid
+    "20% 0%, 80% 0%, 100% 100%, 0% 100%",
+    // Left point
+    "25% 0%, 100% 0%, 100% 100%, 25% 100%, 0% 50%",
+    // Frame
+    // "0% 0%, 0% 100%, 25% 100%, 12% 12%, 92% 11%, 91% 90%, 0 91%, 25% 100%, 100% 100%, 100% 0%",
 ];
 function changeShape() {
-    const randomShape = Math.floor[(Math.random())
-        *shapeArr.length];
-        const newCliPath = shapeArr[randomShape];
-        const currentClipPath = getComputedStyle(img).getPropertyValue("--shape");
-
-        // img.style.setProperty("--shape", shapeAtt[randomShape]);
-
-        if (currentClipPath !== newCliPath) {
-             img.style.setProperty("--shape", newCliPath);
-        } else {
-             img.style.setProperty("--shape", shapeArr[randomShape + 1]);
-             console.log(shapeArr[randomShape])
-        }
+    const randomShape = Math.floor(Math.random()
+    * shapeArr.length);
+    const newClipPart = shapeArr[randomShape]
+    const currentClipPart = getComputedStyle(img).getPropertyValue("--shpae");
+    // img.style.setProperty("--shape", shapeArr[randomShape]);
+    if (currentClipPart !== newClipPart) {
+        img.style.setProperty("--shape", newClipPart)
+    } else {
+        img.style.setProperty("--shape, shapeArr")
+        console.log(shapeArr[randomShape + 1])
+    }
 }
+
+
+
+
+
